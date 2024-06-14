15c03c5d94794366d6fa37140811770a9532b5b3
Benjamin Trueman <40065050+bentrueman@users.noreply.github.com>
2024-06-14 16:06:38 -0300

    updated spotify code

--- R/2024-06-07-tidy-tuesday-spotify.R
+++ R/2024-06-07-tidy-tuesday-spotify.R
@@ -1,7 +1,11 @@
 
 # This week we're attempting an answer to the question, "Did the introduction of Spotify
 # encourage shorter song lengths?". We'll be using a few methods to estimate how song length 
-# changed over time, before and after the founding of Spotify.
+# changed over time, before and after the founding of Spotify:
+
+# 1) a linear model
+# 2) a fixed window moving average
+# 3) an adaptive window moving average
 
 # setup -------------------------------------------------------------------
 
@@ -10,10 +14,7 @@ library("tidytuesdayR")
 
 spotify_founded <- as.Date("2006-04-23") # from https://en.wikipedia.org/wiki/Spotify
 
-theme_set(
-  theme_bw() + 
-    theme(legend.position = "bottom")
-)
+theme_set(theme_bw())
 
 # read data ---------------------------------------------------------------
 
@@ -36,8 +37,7 @@ spotify_songs <- data$spotify_songs |>
 # plot the data -----------------------------------------------------------
 
 p1 <- spotify_songs |> 
-  ggplot(aes(track_album_release_date, duration_min)) + 
-  facet_wrap(vars(playlist_genre)) +
+  ggplot(aes(track_album_release_date, duration_min)) +
   geom_point() +
   geom_vline(aes(xintercept = spotify_founded, col = "Spotify founded")) +
   labs(col = NULL)
@@ -75,3 +75,98 @@ model_2 <- lm(
 p1 + 
   geom_line(aes(y = fitted(model_1), col = "model 1")) +
   geom_line(aes(y = fitted(model_2), col = "model 2"))
+
+# moving average with a fixed window size ---------------------------------
+
+# next, let's look at a more flexible approach - the moving average. for each date in our dataset,
+# we'll estimate the expected song length as the average song length in a two-year window from one year
+# before to one year after that date.
+
+window <- 365 # in days
+
+# define a function to select only songs within our time window, then take the average length:
+
+moving_average_fixed_window <- function(x) {
+  input_fixed_window |> 
+    filter(
+      track_album_release_date > track_album_release_date[x] - window,
+      track_album_release_date < track_album_release_date[x] + window,
+    ) |> 
+    with(mean(duration_min))
+}
+
+# to speed things up, select only the columns we need:
+
+input_fixed_window <- spotify_songs |> 
+  select(track_album_release_date, duration_min)
+
+# map() applies our function to each row of the input dataframe:
+
+output_fixed_window <- map_dbl(
+  seq(nrow(input_fixed_window)), moving_average_fixed_window, .progress = TRUE
+)
+
+# plot the results --------------------------------------------------------
+
+p1 <- p1 +
+  geom_line(aes(y = output_fixed_window, col = "Moving average, fixed window"))
+
+# moving average with an adaptive window ----------------------------------
+
+# the fixed window moving average undersmooths when there aren't many data points.
+# but if we increase the window size we end up oversmoothing when there are lots of data - 
+# try it and see. 
+
+# we'll get better results with an adaptive window, that varies in width according to how much 
+# data we have:
+
+# let's keep 10% of the data the data in our window at all times. we'll 
+# specify this using indices - that is, we'll take an average of the n data points 
+# going back and forward in time
+
+window <- round(0.1 * nrow(spotify_songs)) # n = 10% of the data
+window <- seq(window) # create an index from 1 to n
+window <- window - round(median(window)) # center our window at 0, so we go back n/2 and forward n/2 to calculate our average
+
+# this time, we'll need to put our data in time-order, since we're using positions, not dates, to subset:
+
+input_adaptive_window <- spotify_songs |> 
+  arrange(track_album_release_date) |> 
+  select(track_album_release_date, duration_min)
+
+# our function is similar to moving_average_fixed_window()
+
+moving_average_adaptive_window <- function(x) {
+  keep_these_rows <- x + window
+  # make sure keep_these_rows is positive (we can't include points before the first point) and at most 
+  # equal to the number of rows in the input (the moving average estimate for the last point will only 
+  # include time points in the past)
+  keep_these_rows <- keep_these_rows[keep_these_rows > 0 & keep_these_rows <= nrow(input_adaptive_window)]
+  input_adaptive_window |> 
+    slice(keep_these_rows) |> 
+    with(mean(duration_min))
+}
+
+# again, map() applies our function to each row of the input dataframe:
+
+output_adaptive_window <- map_dbl(
+  seq(nrow(input_adaptive_window)), moving_average_adaptive_window, .progress = TRUE
+)
+
+# add the output to the input:
+
+input_adaptive_window <- input_adaptive_window |> 
+  mutate(output_adaptive_window)
+
+# plot the results --------------------------------------------------------
+
+p1 +
+  geom_line(
+    data = input_adaptive_window,
+    aes(track_album_release_date, output_adaptive_window, col = "Moving average, adaptive window")
+  ) +
+  geom_line(aes(y = fitted(model_2), col = "Linear model")) + 
+  labs(
+    x = "Track release date", 
+    y = "Song length (min.)"
+  )
